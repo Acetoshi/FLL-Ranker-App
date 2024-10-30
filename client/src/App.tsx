f3300147db51922090bada7a94eaa0a1323b6953
JohaneDecamps <johane.decamps@gmail.com>
2024-10-30 14:28:14 +0100

    creation de la barre de navigation de l'organisateur

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,13 +2,13 @@ import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid2";
 import Container from "@mui/material/Container";
 import { Outlet } from "react-router";
-import  NotificationProvider  from "./contexts/NotificationContext";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <NotificationProvider>
+    <>
       <Grid size={12}>
-        <div style={{ backgroundColor: "#ccc" }}>FLL Ranker NAVBAR</div>
+        <NavBar/>
       </Grid>
       <Container fixed>
         <Box sx={{ flexGrow: 1 }}>
@@ -22,7 +22,7 @@ function App() {
       <Grid size={12}>
         <div>FLL Ranker 2024</div>
       </Grid>
-    </NotificationProvider>
+    </>
   );
 }
 
