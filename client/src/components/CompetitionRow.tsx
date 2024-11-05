5be45e6910cc35fc5dff81f1b4957f8eedd1d0f9
scyanth <34488277+scyanth@users.noreply.github.com>
2024-11-05 16:30:24 +0100

    fix desactivation bouton

--- client/src/components/CompetitionRow.tsx
+++ client/src/components/CompetitionRow.tsx
@@ -182,7 +182,7 @@ export default function CompetitionRow({
           ) : displayMode == "edit" ? (
             <>
               <BtnCRUD
-                disabled={false}
+                disabled={handleDisabled()}
                 handleClick={handleEdit}
                 type={"save"}
               />
